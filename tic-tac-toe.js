70f34bbfcc9724582d2c6c6caa1a1c1aa198c3dc
Abigail Anderson <abigailchrisann2@gmail.com>
2024-10-27 20:11:36 -0500

    Exercise 2 Completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,26 @@
 "use strict";
+let currentState=[]
+function clicker(square) {
+    if (currentState.length==0 || currentState=="O")
+    {
+       square.innerHTML="X"
+       currentState[0]="X"
+       square.classList.add("X")
+       
+    }
+    else if (statArr==("X"))
+    {
+        square.innerHTML="O"
+        currentState[0]="O"
+        square.classList.add("O")
+        
+    }
+ }
+
 window.onload=function () {
     let boardSquares=document.getElementById("board").children
     for (let count=0;count<boardSquares.length;count++) {
         boardSquares[count].className="square"
+        boardSquares[count].setAttribute("onclick","clicker(this)")
     }
 }
