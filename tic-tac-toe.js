861044869dbf97702821cad80ddb675b24b6fc06
Abigail Anderson <abigailchrisann2@gmail.com>
2024-10-27 19:58:32 -0500

    Added initial starter files

--- tic-tac-toe.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-window.onload=function () {
-    let boardSquares=document.getElementById("board").children
-    for (let count=0;count<boardSquares.length;count++) {
-        boardSquares[count].className="square"
-    }
-}
